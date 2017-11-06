236c96f9c1a5f115a7f96717d894eef6f616bf18
Patrik Egyed <patrik.egyed@hu.ibm.com>
2017-11-06 14:11:37 +0100

    CHANGE: versioned subfolder prefix and suffix handling + bugfix + extra test cases.

--- get-highest-versioned-subdirectory.sh
+++ get-highest-versioned-subdirectory.sh
@@ -1,54 +1,128 @@
 #!/usr/bin/env bash
 
-if [ "${#}" -ne 1 ]; then
-    echo "Usage: ${0} <parent_of_versioned_directories>" >&2
-    exit 1
-fi
+exit_invalid_arguments=-1
+exit_parent_not_existing=-2
+exit_parent_not_a_directory=-3
 
-parent_directory="${1%/}"
+check_expression() {
+    if [ "${#}" -ne 4 ]; then
+        error "Error: invalid usage of check_expression(), 4 arguments are required (context, expression, expression_text, exit_code), <${#}> was/were provided, <${@}>." ${exit_invalid_arguments}
+    fi
 
-if [ ! -e "${parent_directory}" ]; then
-    echo "Error: parent directory <${parent_directory}> could not be found" >&2
-    exit 1
-fi
+    context=${1}
+    expression=${2}
+    expression_text=${3}
+    exit_code=${4}
 
-if [ ! -d "${parent_directory}" ]; then
-    echo "Error: <${parent_directory}> specified for parent directory is not a directory" >&2
-    exit 1
-fi
+    if [ ! ${2} ]; then
+        error "Error: expression evaluated as false in <${context}> for expression <${expression_text}>" ${4}
+    fi
+}
 
-current_versions=(0 0 0)
-directories=$(find ${parent_directory} -type d -mindepth 1 -maxdepth 1)
-versioned_directory=""
-version_expression="[0-9]\+\(\.[0-9]\+\)*"
+check_input() {
+    check_expression "check_input()" $((${#} == 1)) "${#} == 1" ${exit_invalid_arguments}
 
-for directory in ${directories[@]}; do
-    directory_name="$(basename ${directory})"
-    versioned_directory_match=$(echo "${directory_name}" | grep ".*${version_expression}.*")
+    parent_directory=${1}
 
-    if [ "${directory_name}" == "${versioned_directory_match}" ]; then
-        version=$(echo "${directory_name}" | grep -o "${version_expression}")
-        versions=(${version//./ })
+    if [ ! -e "${parent_directory}" ]; then
+        error "Error: parent directory <${parent_directory}> could not be found" ${exit_parent_not_existing}
+    fi
 
-        subversion_index=0
-        for subversion in "${versions[@]}"; do
-            if [ ${current_versions[subversion_index]} -gt ${subversion} ]; then
-                break
-            elif [ ${current_versions[subversion_index]} -lt ${subversion} ]; then
-                current_versions=("${versions[@]}")
-                versioned_directory="${directory}"
+    if [ ! -d "${parent_directory}" ]; then
+        error "Error: <${parent_directory}> specified for parent directory is not a directory" ${exit_parent_not_a_directory}
+    fi
+}
 
-                break
+error() {
+    if [ "${#}" -ne 2 ]; then
+        echo "Error: invalid usage of error(), 2 arguments are required, ${#} was/were provided."
+        return -1
+    fi
+
+    error_message=${1}
+    exit_code=${2}
+
+    echo "${error_message}" >&2
+    exit ${exit_code}
+}
+
+get_versioned_directory() {
+    check_expression "get_versioned_directory()" $((${#} == 2)) "${#} == 2" ${exit_invalid_arguments}
+
+    parent_directory="${1%/}"
+    prefix_filter="${2}"
+
+    current_prefix=""
+    current_suffix=""
+    current_versions=(0 0 0)
+    directories=$(find ${parent_directory} -type d -mindepth 1 -maxdepth 1)
+    versioned_directory=""
+    version_expression="[0-9]\+\(\.[0-9]\+\)*"
+
+    for directory in ${directories[@]}; do
+        directory_name="$(basename ${directory})"
+        versioned_directory_match=$(echo "${directory_name}" | grep ".*${version_expression}.*")
+
+        if [ "${directory_name}" == "${versioned_directory_match}" ]; then
+            version=$(echo "${directory_name}" | grep -o "${version_expression}")
+
+            prefix=$(echo "${directory_name}" | grep -o ".*${version_expression}")
+            prefix=(${prefix//${version}/})
+
+            suffix=$(echo "${directory_name}" | grep -o "${version_expression}.*")
+            suffix=(${suffix//${version}/})
+
+            versions=(${version//./ })
+
+            if [ "${prefix_filter}" != "" ] && [ "${prefix}" != "${prefix_filter}" ]; then
+                continue
             fi
 
-            subversion_index=$(expr ${subversion_index} + 1)
-        done
-    fi
-done
+            subversion_index=0
+            version_comparison_result=0
+            for subversion in "${versions[@]}"; do
+                if [ ${current_versions[subversion_index]} -gt ${subversion} ]; then
+                    version_comparison_result=1
+                    break
+                elif [ ${current_versions[subversion_index]} -lt ${subversion} ]; then
+                    current_prefix="${prefix}"
+                    current_suffix="${suffix}"
+                    current_versions=("${versions[@]}")
+                    versioned_directory="${directory}"
 
-if [ "$versioned_directory" == "" ]; then
-    echo "Error: could not find any properly versioned directory in $parent_directory" >&2
-    exit 1
+                    version_comparison_result=2
+                    break
+                fi
+
+                subversion_index=$(expr ${subversion_index} + 1)
+            done
+
+            if [ ${version_comparison_result} -eq 0 ] && [ "${current_suffix}" \< "${suffix}" ]; then
+                current_prefix="${prefix}"
+                current_suffix="${suffix}"
+                current_versions=("${versions[@]}")
+                versioned_directory="${directory}"
+            fi
+        fi
+    done
+
+    if [ "${versioned_directory}" == "" ]; then
+        echo "Error: could not find any properly versioned directory in ${parent_directory}" >&2
+        exit 1
+    fi
+
+    echo "${versioned_directory}"
+}
+
+check_expression "${0}" $((${#} >= 1)) "${#} >= 1" ${exit_invalid_arguments}
+
+parent_directory="${1}"
+prefix_filter=""
+
+if [ ${#} > 1 ]; then
+    prefix_filter="${2}"
 fi
 
-echo "$versioned_directory"
+check_input ${parent_directory}
+
+get_versioned_directory ${parent_directory} ${prefix_filter}
--- test-get-highest-versioned-subdirectory.sh
+++ test-get-highest-versioned-subdirectory.sh
@@ -1,31 +1,83 @@
 #!/usr/bin/env bash
 
-test_directory="test-get-highest-versioned-subdirectory-tmp"
+exit_invalid_arguments=-1
 
-if [ ! -e "${test_directory}" ]; then
-    mkdir "./${test_directory}"
-fi
+check_expression() {
+    if [ "${#}" -ne 4 ]; then
+        error "Error: invalid usage of check_expression(), 4 arguments are required (context, expression, expression_text, exit_code), <${#}> was/were provided, <${@}>." ${exit_invalid_arguments}
+    fi
 
-test_cases=(\
+    context=${1}
+    expression=${2}
+    expression_text=${3}
+    exit_code=${4}
+
+    if [ ! ${2} ]; then
+        error "Error: expression evaluated as false in <${context}> for expression <${expression_text}>" ${4}
+    fi
+}
+
+create_directories() {
+    check_expression "create_directories()" $((${#} == 2)) "${#} == 2" ${exit_invalid_arguments}
+
+    array_name=$2[@]
+    temporary_directory="${1}"
+    test_directories=("${!array_name}")
+
+    if [ ! -e "${temporary_directory}" ]; then
+        mkdir "./${temporary_directory}"
+    fi
+
+    for item in ${test_directories[@]}; do
+        mkdir "./${temporary_directory}/${item}"
+    done
+}
+
+error() {
+    if [ "${#}" -ne 2 ]; then
+        echo "Error: invalid usage of error(), 2 arguments are required, ${#} was/were provided."
+        return -1
+    fi
+
+    error_message=${1}
+    exit_code=${2}
+
+    echo "${error_message}" >&2
+    exit ${exit_code}
+}
+
+temporary_directory="test-get-highest-versioned-subdirectory-tmp"
+test_directories=(\
+"test-fake" \
 "test-0.0.1alpha" \
 "test-2.5.30" \
 "test-9.20.0" \
 "test-10.0.1" \
 "test-10.3.10" \
-"test-10.4.0" \
+"test-10.4.0k" \
+"test-10.4.0l" \
+"10.4.0m" \
 )
 
-for item in ${test_cases[@]}; do
-    mkdir "./${test_directory}/${item}"
-done
+create_directories "${temporary_directory}" test_directories
 
-result=$(./get-highest-versioned-subdirectory.sh ${test_directory})
-rm -rf "${test_directory}"
+result_simple=$(./get-highest-versioned-subdirectory.sh ${temporary_directory})
+expected_result_simple="${temporary_directory}/10.4.0m"
 
-if [ "${result}" != "${test_directory}/test-10.4.0" ]; then
+result_filtered=$(./get-highest-versioned-subdirectory.sh ${temporary_directory} "test-")
+expected_result_filtered="${temporary_directory}/test-10.4.0l"
+
+rm -rf "${temporary_directory}"
+
+if [ "${result_simple}" != "${expected_result_simple}" ]; then
     echo "Failed: ${0}"
-    echo "    Actual:   ${result}"
-    echo "    Expected: ${test_directory}/test-10.4.0"
+    echo "    Actual:   ${result_simple}"
+    echo "    Expected: ${expected_result_simple}"
+    exit 1
+elif [ "${result_filtered}" != "${expected_result_filtered}" ]; then
+    echo "Failed: ${0}"
+    echo "    Actual:   ${result_filtered}"
+    echo "    Expected: ${expected_result_filtered}"
     exit 1
 else
     echo "Passed: ${0}"
